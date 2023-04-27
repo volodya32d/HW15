8c855a89a3d0c4dd34117b5084f41ab48c3a78d6
volodya32d <nyznyk.volodymyr@gmail.com>
2023-04-27 22:52:43 +0300

    HW15

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,52 @@
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Number? ");
+        int number = Integer.parseInt(scanner.next());
+
+        if ((number == 9) || (number == 10))
+            System.out.println("Well");
+        else if ((number == 7) || (number == 8))
+            System.out.println("Good");
+        else if(number == 6)
+            System.out.println("Acceptable");
+        else if(number == 5)
+            System.out.println("Bad");
+        else if ((number >= 0) && (number <= 4))
+            System.out.println("Very Bad");
+        else
+            System.out.println("Not valid");
+
+        switch(number)
+        {
+            case 0:
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+                System.out.println("Very Bad");
+                break;
+            case 5:
+                System.out.println("Bad");
+                break;
+            case 6:
+                System.out.println("Acceptable");
+                break;
+            case 7:
+            case 8:
+                System.out.println("Good");
+                break;
+            case 9:
+            case 10:
+                System.out.println("Well");
+                break;
+            default:
+                System.out.println("Not valid");
+                break;
+        }
     }
 }
